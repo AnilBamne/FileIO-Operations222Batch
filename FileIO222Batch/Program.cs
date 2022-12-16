727c0098469aaa722103cd6693c4f96af1f63009
AnilBamne <anilbamne20@gmail.com>
2022-12-16 16:57:41 +0530

    Adding:WelcomeMessage

---
+++ FileIO222Batch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileIO222Batch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to File IO operations programs");
+        }
+    }
+}
